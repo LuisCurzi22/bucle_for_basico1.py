54af9965298c8e15ff09e8f747d6adf813265e60
LuisCurzi <luiscurzi22@gmail.com>
2026-04-15 17:02:56 -0300

    segundo commit

---
+++ funciones_basicas_1.py
@@ -0,0 +1,43 @@
+#1
+#def cantidad_de_vocales():
+#    return 5
+#print(cantidad_de_vocales())
+
+#2
+#def cantidad_de_glaciares_argentina():
+#    return 16968
+#def cantidad_de_dias_o_meses_o_semanas_en_anio():
+#    return 365
+#print(cantidad_de_dias_o_meses_o_semanas_en_anio() + cantidad_de_glaciares_argentina())
+
+#3
+#def anio_independencia_chile():
+#    return 1810
+
+#print(anio_independencia_chile())
+
+#def cantidad_de_departamentos_colombia():
+#    print(33)
+#    return 32
+
+#print(cantidad_de_departamentos_colombia())
+
+#5
+#def altura_machu_picchu():
+#    return(2438)
+
+#x = altura_machu_picchu()
+#print(x)
+
+#6
+#def suma(a, b):
+#    return a + b
+
+#print(suma(10, 5) + suma(4, 3))
+
+7
+#def concatenar(a, b):
+#    return str(b) + str(a)
+
+#print(concatenar(7, 15))
+
